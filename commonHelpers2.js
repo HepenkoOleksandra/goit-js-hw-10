a314ee7e57e3e5a99e9c3167e1635b5055e82727
HepenkoOleksandra <144049737+HepenkoOleksandra@users.noreply.github.com>
2024-02-05 14:01:10 +0000

    Deploying to gh-pages from @ HepenkoOleksandra/goit-js-hw-10@920367d5b0b13ea75f8570e6aa95312491b03219 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";import"./assets/styles-4ed993c7.js";document.querySelector(".form");
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */document.querySelector(".form");
 //# sourceMappingURL=commonHelpers2.js.map
