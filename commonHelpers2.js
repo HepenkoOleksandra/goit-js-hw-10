2197d00406e7038799fc2e7561d89f840928c54a
HepenkoOleksandra <144049737+HepenkoOleksandra@users.noreply.github.com>
2024-02-07 20:31:58 +0000

    Deploying to gh-pages from @ HepenkoOleksandra/goit-js-hw-10@11524ca5a58c7b55981833699b4e4dc6626288ec 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */document.querySelector(".form");
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */document.querySelector(".form");const o=new Promise((e,s)=>{setTimeout(()=>{e("Success! Value passed to resolve function")},5e3)});console.log(o);
 //# sourceMappingURL=commonHelpers2.js.map
