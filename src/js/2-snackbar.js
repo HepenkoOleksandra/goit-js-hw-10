11524ca5a58c7b55981833699b4e4dc6626288ec
HepenkoOleksandra <gepenkoalex@gmail.com>
2024-02-07 22:31:05 +0200

    'update'

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,15 @@
 const form = document.querySelector(".form");
+
+const isSuccess = true;
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (isSuccess) {
+      resolve("Success! Value passed to resolve function");
+    } else {
+      reject("Error! Error passed to reject function");
+    }
+  }, 5000);
+});
+
+console.log(promise);
