34facee5c496dd4fe886543df278a259db056267
HepenkoOleksandra <gepenkoalex@gmail.com>
2024-02-10 13:49:59 +0200

    'update'

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,15 +1,60 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
 const form = document.querySelector(".form");
 
-const isSuccess = true;
+form.addEventListener('submit', onFormSubmit);
 
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (isSuccess) {
-      resolve("Success! Value passed to resolve function");
-    } else {
-      reject("Error! Error passed to reject function");
-    }
-  }, 5000);
-});
+function onFormSubmit(e) {
+  e.preventDefault();
 
-console.log(promise);
+  const delay = e.target.elements.delay.value.trim();
+  const state = e.target.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then((delay) => {
+      iziToast.show({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        messageColor: 'white',
+        backgroundColor: 'green',
+        position: 'topRight',
+      })
+    })
+    .catch((delay) => {
+      iziToast.show({
+        message: `❌ Rejected promise in ${delay}ms`,
+        messageColor: 'white',
+        backgroundColor: 'red',
+        position: 'topRight',
+      });
+    });
+
+  e.target.reset();
+}
+
+
+
+
+// const isSuccess = true;
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (isSuccess) {
+//       resolve("Success! Value passed to resolve function");
+//     } else {
+//       reject("Error! Error passed to reject function");
+//     }
+//   }, 5000);
+// });
+
+// console.log(promise);
